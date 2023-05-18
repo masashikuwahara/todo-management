4d4f3dd5b061f44886a9967631c835997d5a4e88
masashikuwahara <brucemclarenp4@gmail.com>
2023-05-18 12:21:36 +0900

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:edit, :update, :destroy, :toggle]
+    def index
+        @task = Task.all
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+        redirect_to root_path
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @task.update(task_params)
+            redirect_to root_path
+        else
+            render 'edit', status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @task.destroy
+        redirect_to root_path, status: :see_other
+    end
+
+    def toggle
+        @task.update(completed: !@task.completed)
+        render turbo_stream: turbo_stream.replace(
+            @task,
+            partial: 'completed',
+            locals: { task: @task}
+        )
+    end
+
+    private
+    def task_params
+        params.require(:task).permit(:title)
+    end
+
+    def set_task
+        @task = Task.find(params[:id])
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  resources :tasks do
+    member do
+      patch :toggle
+    end
+  end
+  root 'tasks#index'
+end
